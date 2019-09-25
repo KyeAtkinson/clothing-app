3318a2a7f67c2d8585bdf33afdaa54ba37f92d67
Kye <kye@BGGLOBAL9334.local>
2019-09-25 14:30:40 +0100

    Added Register and Email Sign-up

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,31 @@ const config = {
   messagingSenderId: "100407217569",
   appId: "1:100407217569:web:15bee7366002d654464fe0",
   measurementId: "G-CTFRMR4T7Y"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+  return userRef;
 }
 
 firebase.initializeApp(config);
